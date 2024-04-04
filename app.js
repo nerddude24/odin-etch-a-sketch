c1ad1c6cb6312da0d8b7477b2584c3375bc4ace0
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-04-04 23:38:00 +0100

    Add correct 16x16 grid with styling

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i < 16; i++) {
+	let gridRow = document.createElement("div");
+	gridRow.className = "gridRow";
 	for (let j = 0; j < 16; j++) {
 		let div = document.createElement("div");
-		div.className = "gridTile";
+		div.className = "gridItem";
 
-		container.appendChild(div);
+		gridRow.appendChild(div);
 	}
+
+	container.appendChild(gridRow);
 }
