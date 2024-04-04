618b59ca5d4ed48a265113333450e0dba2e6b53f
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-04-04 23:16:22 +0100

    Add script for adding 16x16 grid of divs

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const container = document.querySelector(".container");
+
+for (let i = 0; i < 16; i++) {
+	for (let j = 0; j < 16; j++) {
+		let div = document.createElement("div");
+		div.className = "gridTile";
+
+		container.appendChild(div);
+	}
+}
