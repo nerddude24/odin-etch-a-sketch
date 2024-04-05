5b301be52ab2bd8c682cbc6ed1c497d9b4014836
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-04-05 11:26:05 +0100

    Add clearGrid and createGrid funcs

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-	let gridRow = document.createElement("div");
-	gridRow.className = "gridRow";
-	for (let j = 0; j < 16; j++) {
-		let div = document.createElement("div");
-		div.classList.add("gridItem");
-
-		div.addEventListener("mouseenter", (e) => {
-			e.target.classList.add("gridItemActivated");
-		});
-
-		gridRow.appendChild(div);
-	}
-
-	container.appendChild(gridRow);
+function clearGrid() {
+	container.innerHTML = "";
 }
+
+function createGrid(size) {
+	clearGrid();
+
+	for (let i = 0; i < size; i++) {
+		let gridRow = document.createElement("div");
+		gridRow.className = "gridRow";
+		for (let j = 0; j < size; j++) {
+			let div = document.createElement("div");
+			div.classList.add("gridItem");
+
+			div.addEventListener("mouseenter", (e) => {
+				e.target.classList.add("gridItemActivated");
+			});
+
+			gridRow.appendChild(div);
+		}
+
+		container.appendChild(gridRow);
+	}
+}
+
+createGrid(32);
